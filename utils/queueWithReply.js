46f7d0d24ad4477c490fc7b03110c70f0dfaeec1
Kentrell <graylight2607@gmail.com>
2025-12-22 21:49:52 -0500

    yt

--- utils/queueWithReply.js
+++ utils/queueWithReply.js
@@ -1,16 +1,8 @@
+// utils/queueWithReply.js
 const { enqueueInteraction } = require('../queue');
 
 async function queueWithReply(interaction, extra = {}) {
-  // 💥 only defer if we're running on real Discord interaction
-  if (typeof interaction?.deferReply === 'function') {
-    try {
-      await interaction.deferReply();
-    } catch (e) {
-      console.warn('⚠️ Could not defer reply:', e.message);
-    }
-  }
-
-  return enqueueInteraction(interaction, extra);
+  return await enqueueInteraction(interaction, extra);
 }
 
 module.exports = { queueWithReply };
