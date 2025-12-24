e6de220086f73dcb497a512cd594379f51fde05c
Kentrell <graylight2607@gmail.com>
2025-12-23 19:25:25 -0500

    yuuu

--- worker-commands/card-create.js
+++ worker-commands/card-create.js
@@ -65,15 +65,20 @@ const safeFollowUp = async (data) => {
       console.error('[CARD-CREATE] ❌ Failed to editReply:', e);
     }
 
-    const allowedRole = String(process.env.ALLOWED_ROLE_ID);
+    const allowedRole = String(process.env.CARD_CREATOR_ROLE_ID);
 
-    console.log('[DEBUG] allowedRole:', allowedRole);
-console.log('[DEBUG] hasRole:', interaction.member.roles.cache.has(allowedRole));
-console.log('[DEBUG] role cache:', [...interaction.member.roles.cache].map(([id]) => id));
+const hasRole = interaction.member?.roles?.cache?.has(allowedRole);
 
-if (!interaction.member.roles?.cache?.has(allowedRole)) {
-  return safeEdit({ content: 'You do not have permission to run this command.', ephemeral: true });
+console.log('[DEBUG] allowedRole:', allowedRole);
+console.log('[DEBUG] hasRole:', hasRole);
+
+if (!hasRole) {
+  return safeEdit({
+    content: 'You do not have permission to use this command.',
+    ephemeral: true
+  });
 }
+
 
     const opts = interaction.options;
     const cardCode = opts.getString('cardcode');
