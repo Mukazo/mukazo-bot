1d507eb4f221f3d2a37ededeaef0224075ba1176
Kentrell <graylight2607@gmail.com>
2025-12-30 05:19:39 -0500

    efwjef

--- commands/global/cooldowns.js
+++ commands/global/cooldowns.js
@@ -43,7 +43,7 @@ module.exports = {
       .setThumbnail(interaction.user.displayAvatarURL({ dynamic: true }))
       .setDescription(
         [
-          '### <:space:1455504212069842956>˖ ࣪⊹ My Cooldowns',
+          '### ˖ ࣪⊹ My Cooldowns',
           '',
           '',
           description,
