d107c605801c1a156ffec6f2c1d630ac461daa22
Kentrell <graylight2607@gmail.com>
2026-01-01 02:02:39 -0500

    ewro

--- worker-commands/gift.js
+++ worker-commands/gift.js
@@ -1,45 +1,46 @@
 const CardInventory = require('../models/CardInventory');
 const User = require('../models/User');
 
+module.exports = {
+  async execute(data) {
+    const { from, to, cards, wirlies } = data;
 
-module.exports = async function giftWorker(job) {
-  const { from, to, cards, wirlies } = job.data;
+    for (const { cardCode, qty } of cards) {
+      const dec = await CardInventory.findOneAndUpdate(
+        { userId: from, cardCode, quantity: { $gte: qty } },
+        { $inc: { quantity: -qty } },
+        { new: true }
+      );
 
-  for (const { cardCode, qty } of cards) {
-    const dec = await CardInventory.findOneAndUpdate(
-      { userId: from, cardCode, quantity: { $gte: qty } },
-      { $inc: { quantity: -qty } },
-      { new: true }
-    );
+      if (!dec) throw new Error(`Not enough copies of ${cardCode}`);
 
-    if (!dec) throw new Error(`Not enough copies of ${cardCode}`);
+      if (dec.quantity <= 0) {
+        await CardInventory.deleteOne({ userId: from, cardCode });
+      }
 
-    if (dec.quantity <= 0) {
-      await CardInventory.deleteOne({ userId: from, cardCode });
+      await CardInventory.updateOne(
+        { userId: to, cardCode },
+        { $setOnInsert: { userId: to, cardCode }, $inc: { quantity: qty } },
+        { upsert: true }
+      );
     }
 
-    await CardInventory.updateOne(
-      { userId: to, cardCode },
-      { $setOnInsert: { userId: to, cardCode }, $inc: { quantity: qty } },
-      { upsert: true }
-    );
-  }
+    if (wirlies > 0) {
+      const sender = await User.findOneAndUpdate(
+        { userId: from, wirlies: { $gte: wirlies } },
+        { $inc: { wirlies: -wirlies } },
+        { new: true }
+      );
 
-  if (wirlies > 0) {
-    const sender = await User.findOneAndUpdate(
-      { userId: from, wirlies: { $gte: wirlies } },
-      { $inc: { wirlies: -wirlies } },
-      { new: true }
-    );
+      if (!sender) throw new Error('Not enough Wirlies.');
 
-    if (!sender) throw new Error('Not enough Wirlies.');
+      await User.updateOne(
+        { userId: to },
+        { $inc: { wirlies } },
+        { upsert: true }
+      );
+    }
 
-    await User.updateOne(
-      { userId: to },
-      { $inc: { wirlies } },
-      { upsert: true }
-    );
-  }
-
-  return { ok: true };
+    return { ok: true };
+  },
 };
