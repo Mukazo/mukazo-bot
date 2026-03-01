1ebb6c3d930fc3039b98e1e4b8a0d01b7088bd91
Kentrell <graylight2607@gmail.com>
2026-02-28 21:40:02 -0500

    fejw

--- commands/guild-only/brew.js
+++ commands/guild-only/brew.js
@@ -5,7 +5,7 @@ const CardInventory = require('../../models/CardInventory');
 
 // Use Discord Role IDs for accurate tier detection
 const ROLE_TIERS = {
-  '1465789192326873231': { name: 'Pixie', limit: 7, chance: 0.65 },
+  '1465789192326873231': { name: 'Pixie', limit: 7, chance: 0.6 },
   '1447006809419415622': { name: 'Stardust', limit: 5, chance: 0.55 },
   '1447006766733725747': { name: 'Ethereal', limit: 3, chance: 0.5 },
   '1447006737042378772': { name: 'Daydream', limit: 2, chance: 0.45 }
